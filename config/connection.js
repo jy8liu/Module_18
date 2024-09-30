61fdfde9c0d5529f8aa52fb3d89bc464eabb3a6a
jy8liu <jy8liu@gmail.com>
2024-09-29 20:26:24 -0700

    final

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const { connect, connection } = require('mongoose');
 
-const connectionString = 'mongodb://127.0.0.1:27017/studentsDB';
+const connectionString = 'mongodb+srv://sirminderbinder:6W6P061oMs7Y9ijC@cluster0.eyf9t.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
 connect(connectionString);
 
